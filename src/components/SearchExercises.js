6a5e58170ed1b1e9c86060c7b0e24977f6b283ac
opyvovarova <opyvovarova087@gmail.com>
2022-06-27 11:07:48 +0300

    update files

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography 
+       fontWeight={700}
+       sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+       mb="49px" textAlign="center"
+      >
+        Awesome Exercises You < br/> Should Know
+      </Typography>
+    </Stack>
   )
 }
 
