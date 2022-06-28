4faad97574e5b215c51507981a33c6592f4bafd2
opyvovarova <opyvovarova087@gmail.com>
2022-06-28 23:07:35 +0300

    Update files and create fetchData fot search

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,29 +5,31 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
-   <Box sx={{
+   <Box width="100%" alignItems="center" textAlign="right" sx={{
     mt: { lg: '212px', xs: '70px'},
     ml: { sm: '50px'} 
   }} position="relative" p="20px">
-    <Typography color="#FF2625"
-    fontWeight="600" fontSize="26px">
+    <Typography color="#f74bb5"
+    fontWeight="600" fontSize="26px" >
      Fitness Club
     </Typography>
     <Typography fontWeight={700} sx={{
       fontSize: { lg: '44px', xs: '40px' }
-    }} mb="23px" mt="30px">
+    }} mb="23px" mt="30px"  color="#fff">
       Sweat, Smile < br/> and Repeat
     </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" color="#fff">
       Check out the most effective exercises
     </Typography>
-    <Stack>
-        <a href='#exercises' style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
-    </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{
+    <Box  sx={{ mr: 0 }} className="btn-herobanner">
+        <a href='#exercises'   style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#f74bb5', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Box>
+    <Typography fontWeight={600}  color="#f74bb5" sx={{
       opacity: '0.1',
+      textAlign: 'right',
       display: { lg: 'block', xs: 'none' },
-      fontSize: '200px'
+      fontSize: '200px',
+    
     }}>
       Exercise
     </Typography>
